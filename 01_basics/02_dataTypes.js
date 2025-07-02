9130dfa7aec3d31037bc8daa9e367e8592cc7599
munishsingh77 <you@example.com>
2025-07-02 16:22:55 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+let name = "Munish";
+let age = 25;
+let isLoggedIn = true;
+
+
+// number => 2 to power 53
+// BigInt
+// string
+// boolean => true or false 
+// null => standalone value
+// undefine 
+// Symbol => unique 
+
+//object
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof null); // object (this is a known JavaScript quirk)
+console.log(typeof undefined); // undefined
+console.log(typeof Symbol("id")); // symbol
